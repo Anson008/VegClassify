0bb026e8ab4035003d5c686b5a35d98b00fff062
Xingchen Zhao <xczhao007@gmail.com>
2023-07-23 23:12:02 -0600

    Add browser control module

--- webscraper.py
+++ webscraper.py
@@ -8,8 +8,8 @@ options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrom
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get('https://livingatlas.arcgis.com/wayback/#active=47568&ext=-111.81786,41.74544,-111.81531,41.74735')
-#driver.find_element(By.LINK_TEXT, '2021-08-11').click()
+# driver.find_element(By.LINK_TEXT, '2021-08-11').click()
 
 print("Chrome Browser Invoked")
-#driver.close()
+# driver.close()
 
