531ca15047540317844c1735ed24d08f775c5619
Xingchen Zhao <xczhao007@gmail.com>
2023-07-28 20:08:42 -0600

    Reproject NAIP to CRS to find longitude and latitude of each pixel

--- ndvi.py
+++ ndvi.py
@@ -12,26 +12,30 @@ import pandas as pd
 # Read NAIP data from file into xarray.DataArray
 img_path = "./image/m_4111118_nw_12_060_20210813_Clip.tif"
 naip_img = rxr.open_rasterio(img_path)
-
 print("NAIP image shape: ", naip_img.shape)
 
+# Reproject NAIP to EPSG:4326
+num_bands, height, width = naip_img.shape
+naip_reproj = naip_img.rio.reproject("EPSG:4326", shape=(height, width))
+
+
 # Convert xarray.DataArray to numpy array
-naip_img = naip_img.to_numpy()
+naip_reproj_arr = naip_reproj.values
 
 # Extract RGB channels and reorder the color axis from (c, w, h) to (w, h, c)
-naip_rgb = np.moveaxis(naip_img[0:3], 0, -1)
+naip_rgb = np.moveaxis(naip_reproj_arr[0:3], 0, -1)
 print("NAIP RGB image shape:", naip_rgb.shape)
 
 
 # Convert RGB to BGR, as BGR is the default color model of OpenCV
 naip_rgb = cv2.cvtColor(naip_rgb, cv2.COLOR_RGB2BGR)
 # cv2.imwrite("./image/naip_rgb.png", naip_rgb)
-# cv2.imshow("NAIP RGB Image", naip_rgb)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.imshow("NAIP RGB Image", naip_rgb)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # Cast uint8 to float64 to prevent calculation from overflowing
-naip_data = naip_img.astype(np.float64)
+naip_data = naip_reproj_arr.astype(np.float64)
 
 # Calculate NDVI. Input must be numpy array. xarray does not support 2D boolean indexing
 naip_ndvi = es.normalized_diff(naip_data[3], naip_data[0])
@@ -83,9 +87,11 @@ area_df = pd.DataFrame(stats[:, [cv2.CC_STAT_WIDTH, cv2.CC_STAT_HEIGHT, cv2.CC_S
 area_stats = area_df.describe()
 print(area_stats.round(2))
 
+width_filter = 3
+height_filter = 3
 keep_count = 0
 video_frames = []
-component_masks = dict()
+component_masks = dict()  # key: label; value: mask value
 for i in range(1, num_labels):
     x = stats[i, cv2.CC_STAT_LEFT]
     y = stats[i, cv2.CC_STAT_TOP]
@@ -94,8 +100,8 @@ for i in range(1, num_labels):
     area = stats[i, cv2.CC_STAT_AREA]
     (c_x, c_y) = centroids[i]
 
-    keep_w = w > 10
-    keep_h = h > 10
+    keep_w = w >= width_filter
+    keep_h = h >= height_filter
 
     if all((keep_w, keep_h)):
         keep_count += 1
@@ -127,27 +133,35 @@ for i, (key, value) in enumerate(component_masks.items()):
     samples = np.where(value == 255)
     j = np.random.randint(0, len(samples[0]))
     cc_random_location[i, 0] = key
-    cc_random_location[i, 1] = samples[1][j]  # Column index: x or width
-    cc_random_location[i, 2] = samples[0][j]  # Row index: y or height
+    lon = naip_reproj[0, samples[0][j], samples[1][j]].x.values  # samples[0]: y or height; samples[1]: x or width
+    lat = naip_reproj[0, samples[0][j], samples[1][j]].y.values
+    cc_random_location[i, 1] = lon
+    cc_random_location[i, 2] = lat
 
-cc_random_location_df = pd.DataFrame(cc_random_location, columns=['Label', 'randLocX', 'randLocY'])
+cc_random_location_df = pd.DataFrame(cc_random_location, columns=['Label', 'RandLocLon', 'RandLocLat'])
 cc_labels = np.arange(num_labels).reshape((num_labels, 1))
+cc_lat_lon = naip_reproj
 cc_info = np.hstack((cc_labels, stats, centroids))
 cc_info_df = pd.DataFrame(cc_info, columns=['Label', 'LeftMost', 'TopMost', 'Width',
                                             'Height', 'Area', 'CentroidsX', 'CentroidsY'])
 
-cc_info_df = cc_info_df.set_index('Label').join(cc_random_location_df.set_index('Label'), how='inner')
-
-# Sort by area
+cc_info_df['TopLeftLon'] = naip_reproj[0, stats[:, cv2.CC_STAT_TOP], stats[:, cv2.CC_STAT_LEFT]].x.values
+cc_info_df['TopLeftLat'] = naip_reproj[0, stats[:, cv2.CC_STAT_TOP], stats[:, cv2.CC_STAT_LEFT]].y.values
 cc_info_df_sorted = cc_info_df.sort_values(by=['Area'])
 
+cc_info_random_pos_df = cc_info_df.set_index('Label').join(cc_random_location_df.set_index('Label'), how='inner')
+
+# Sort by area
+cc_info_random_pos_df_sorted = cc_info_random_pos_df.sort_values(by=['Area'])
+
 # Filter out based on width and height
-cc_info_df_filtered = cc_info_df_sorted.loc[(cc_info_df_sorted['Width'] > 10) &
-                                            (cc_info_df_sorted['Height'] > 10) &
-                                            (cc_info_df_sorted['LeftMost'] > 0) &
-                                            (cc_info_df_sorted['TopMost'] > 0)]
+cc_info_random_pos_df_filtered = cc_info_random_pos_df_sorted.loc[
+    (cc_info_random_pos_df_sorted['Width'] >= width_filter) &
+    (cc_info_random_pos_df_sorted['Height'] >= height_filter) &
+    (cc_info_random_pos_df_sorted['LeftMost'] > 0) &
+    (cc_info_random_pos_df_sorted['TopMost'] > 0)]
 
 # Save to file
-cc_info_df_sorted.to_csv("connected_components_info_all_01.csv", index=False)
-cc_info_df_filtered.to_csv("connected_components_info_filtered_01.csv", index=False)
+cc_info_df_sorted.to_csv("connected_components_info_all_03.csv", index=False)
+cc_info_random_pos_df_filtered.to_csv("connected_components_info_filtered_03.csv", index=False)
 
