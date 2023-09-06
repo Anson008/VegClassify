27069cee6d67d2953c3245cab8bb80cfdde7309b
Xingchen Zhao <xczhao007@gmail.com>
2023-09-06 09:31:10 -0600

    Implement criteria using filter pattern

--- criteria.py
+++ criteria.py
@@ -1,44 +1,80 @@
+import cv2
 from abc import ABC, abstractmethod
 
 
 class CriteriaBase(ABC):
-    def __init__(self, param, target):
-        self._param = param
+    param_dict = {"width": cv2.CC_STAT_WIDTH,
+                  "height": cv2.CC_STAT_HEIGHT,
+                  "area": cv2.CC_STAT_AREA}
+
+    def __init__(self, param_name, target):
+        self._param_name = param_name
         self._target = target
 
     @abstractmethod
-    def produce_criteria(self):
+    def filter(self, connected_component):
         pass
 
 
 class GreaterThanOrEqualToCriteria(CriteriaBase):
 
-    def produce_criteria(self):
-        return self._param >= self._target
+    def filter(self, connected_component):
+        cv2_param = self.param_dict[self._param_name]
+        res = []
+        for i in range(1, connected_component.num_labels):
+            param = connected_component.stats[i, cv2_param]
+            if param >= self._target:
+
+                res.append((connected_component.labels[i], connected_component.centroids[i]))
+        return res
 
 
 class GreaterThanCriteria(CriteriaBase):
-    def produce_criteria(self):
-        return self._param > self._target
+    def filter(self, connected_component):
+        cv2_param = self.param_dict[self._param_name]
+        res = []
+        for i in range(1, connected_component.num_labels):
+            param = connected_component.stats[i, cv2_param]
+            if param > self._target:
+                res.append((connected_component.labels[i], connected_component.centroids[i]))
+        return res
 
 
 class LessThanOrEqualToCriteria(CriteriaBase):
 
-    def produce_criteria(self):
-        return self._param <= self._target
+    def filter(self, connected_component):
+        cv2_param = self.param_dict[self._param_name]
+        res = []
+        for i in range(1, connected_component.num_labels):
+            param = connected_component.stats[i, cv2_param]
+            if param <= self._target:
+                res.append((connected_component.labels[i], connected_component.centroids[i]))
+        return res
 
 
 class LessThanCriteria(CriteriaBase):
-    def produce_criteria(self):
-        return self._param < self._target
+    def filter(self, connected_component):
+        cv2_param = self.param_dict[self._param_name]
+        res = []
+        for i in range(1, connected_component.num_labels):
+            param = connected_component.stats[i, cv2_param]
+            if param < self._target:
+                res.append((connected_component.labels[i], connected_component.centroids[i]))
+        return res
 
 
-class AndCriteria(CriteriaBase):
-    def produce_criteria(self):
-        return self._param and self._target
+class AndCriteria:
+
+    def __init__(self, criteria1, criteria2):
+        self._criteria1 = criteria1
+        self._criteria2 = criteria2
+
+    def apply_criteria(self, cc_result):
+        res_1 = self._criteria1.filter(cc_result)
+        return self._criteria2
 
 
-class OrCriteria(CriteriaBase):
-    def produce_criteria(self):
-        return self._param or self._target
+class OrCriteria:
+    def make_criteria(self):
+        pass
 
