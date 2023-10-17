4f083c6cb4f68d3052bbee7391b4e0862569147e
Xingchen Zhao <xczhao007@gmail.com>
2023-10-17 16:30:09 -0600

    Handle accepting cookies and new html structure for release dates

--- webcontroller.py
+++ webcontroller.py
@@ -6,28 +6,41 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import re
-import shutil
-from screenshotone import Client, TakeOptions
+import pyscreenshot as imagegrab
+import time
 
+# Specify Chrome driver path
 options = Options()
 options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+
+# Start Chrome
 driver = webdriver.Chrome()
 driver.maximize_window()
-url = 'https://livingatlas.arcgis.com/wayback/#active=47568&ext=-111.81786,41.74544,-111.81531,41.74735'
+
+# Load the target webpage
+url = 'https://livingatlas.arcgis.com/wayback/#active=47963&mapCenter=-111.821131%2C41.743871%2C19'
 driver.get(url)
 print("Chrome Browser Invoked")
 
-element = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-    (By.XPATH, "//div[starts-with(@class, 'list-card trailer-half')]")))
+# WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "flex items-center justify-center cursor-pointer my-1")))
+# local_change_check_box = driver.find_element(By.CLASS_NAME, "flex items-center justify-center cursor-pointer my-1")
+# if local_change_check_box.is_selected():
+#     print("Selected. Toggle off.")
+#     local_change_check_box.click()
 
-# dates = driver.find_elements(By.XPATH, "//div[@class='list-card trailer-half']")
+# Wait until the button "Accept Cookies" appears and click
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]'))).click()
+
+# Wait until the list view options appears for parsing release dates of maps
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "py-1")))
+
+# Find all html tags containing the info of release dates
 page_source = driver.page_source
 bs = BeautifulSoup(page_source, 'html.parser')
-# print(bs)
-list_cards = bs.findAll('div', {'class': re.compile('list-card trailer-half+')})
+list_cards = bs.findAll('div', {'class': 'py-1'})
 print(f"Number of date items: {len(list_cards)}")
 
-# Extract map release d
+# Build a hashmap of release date and data-release-num
 date_to_release_num = dict()
 for card in list_cards:
     date = card.find('a', {'class': 'margin-left-half link-light-gray cursor-pointer'})
@@ -36,25 +49,14 @@ for card in list_cards:
 for key, value in date_to_release_num.items():
     print(f"Date: {key}; Release num: {value}")
 
-# access_key = "Tw2yVHATZ8Bj5g"
-# secret_key = "oo1iz_2kDB0fuQ"
-# client = Client(access_key, secret_key)
-#
-# options = (TakeOptions.url(url)
-#            .format("png")
-#            .viewport_width(1920)
-#            .viewport_height(1080)
-#            .block_cookie_banners(True)
-#            .block_chats(True))
-#
-# screenshot = client.take(options)
-#
-# with open("world_imagery_shot.png", "wb") as shot_file:
-#     shutil.copyfileobj(screenshot, shot_file)
+# Wait until the page is fully loaded
+WebDriverWait(driver, 10).until(lambda driver1: driver1.execute_script('return document.readyState') == 'complete')
 
-element = (WebDriverWait(driver, 10)
-           .until(lambda driver1: driver1.execute_script('return document.readyState') == 'complete'))
+# Take a screenshot of the region of interest
+im = imagegrab.grab(bbox=(700, 150, 3840, 2160))
+im.show()
+# im.save("./results/world_imagery_screenshot.png")
 
-driver.save_screenshot("world_imagery_shot.png")
+# driver.save_screenshot("world_imagery_shot.png")
 # driver.close()
 
