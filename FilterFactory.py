beb4e166df8fd7cbd4c50e332f858f9cbf828bd6
Xingchen Zhao <xczhao007@gmail.com>
2023-10-30 15:33:08 -0600

    Implement FilterFactory

---
+++ FilterFactory.py
@@ -0,0 +1,13 @@
+import criteria
+
+
+class FilterFactory:
+    def get_filter(self, field, operator, threshold):
+        if operator == ">=":
+            return criteria.GreaterThanOrEqualToCriteria(field, threshold)
+        if operator == ">":
+            return criteria.GreaterThanCriteria(field, threshold)
+        if operator == "<=":
+            return criteria.LessThanOrEqualToCriteria(field, threshold)
+        if operator == "<":
+            return criteria.LessThanCriteria(field, threshold)
