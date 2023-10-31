4da14745de90a88910e8f6c1d3e3b9e3fe085526
Xingchen Zhao <xczhao007@gmail.com>
2023-10-31 12:56:37 -0600

    Implement Point class and get_screen_resolution function

--- utilities.py
+++ utilities.py
@@ -1,4 +1,5 @@
 import math
+import ctypes
 
 
 class UnitConverterFactory:
@@ -24,4 +25,37 @@ class RadianToDegree:
     @staticmethod
     def get_factor():
         return 360.0 / (2 * math.pi)
+
+
+def get_screen_resolution():
+    user32 = ctypes.windll.user32
+    user32.SetProcessDPIAware()
+    width = user32.GetSystemMetrics(0)
+    height = user32.GetSystemMetrics(1)
+    return width, height
+
+
+class Point:
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    def __str__(self):
+        return f"Point ({self.x}, {self.y}"
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, val):
+        self._x = val
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, val):
+        self._y = val
 
