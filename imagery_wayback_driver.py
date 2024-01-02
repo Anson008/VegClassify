a568593704730b5d86eb5cb693af41939d823bfe
Xingchen Zhao <xczhao007@gmail.com>
2024-01-02 14:25:24 -0700

    Refactor code that extracting map from world imagery wayback

---
+++ imagery_wayback_driver.py
@@ -0,0 +1,98 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import pyscreenshot as imagegrab
+import utilities
+from utilities import Point
+
+
+class ImageryWaybackDriver:
+    def __init__(self, webdriver):
+        self.webdriver = webdriver
+
+    def load_url(self, url):
+        # Load the target webpage
+        self.webdriver.get(url)
+        self.webdriver.fullscreen_window()
+
+    def toggle_off_version_filter(self):
+        # Wait until version filter button is clickable and toggle it off
+        (WebDriverWait(self.webdriver, 10)
+         .until(EC.element_to_be_clickable((By.XPATH, "//*[@icon='check-square']")))
+         .click())
+
+    def close_modal(self):
+        # Wait until the modal window is loaded and click close
+        (WebDriverWait(self.webdriver, 10)
+         .until(EC.element_to_be_clickable((By.XPATH, "//*[@aria-label='close-modal']")))
+         .click())
+
+    def accept_cookies(self):
+        # Wait until the button "Accept Cookies" appears and click
+        (WebDriverWait(self.webdriver, 10)
+         .until(EC.element_to_be_clickable((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')))
+         .click())
+
+    def get_release_dates(self):
+        # Wait until the list view options appears for parsing release dates of maps
+        WebDriverWait(self.webdriver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "py-1")))
+
+        # Find all html tags containing the info of release dates
+        page_source = self.webdriver.page_source
+        bs = BeautifulSoup(page_source, 'html.parser')
+        list_cards = bs.findAll('div', {'class': 'py-1'})
+        print(f"Number of date items: {len(list_cards)}")
+
+        # Build a hashmap of release date and data-release-num
+        date_to_release_num = dict()
+        for card in list_cards:
+            date = card.find('a', {'class': 'margin-left-half link-light-gray cursor-pointer'})
+            date_to_release_num[date.get_text()] = card.attrs['data-release-num']
+        return date_to_release_num
+
+    def take_screenshot(self, width, height, save_to_file):
+        # Wait until the page is fully loaded
+        WebDriverWait(self.webdriver, 10).until(lambda driver1: driver1.execute_script('return document.readyState') == 'complete')
+
+        screen_w, screen_h = utilities.get_screen_resolution()
+        nav_width = 350
+        browser_header_size = 0  # Equal to 0 when full screen
+        center = Point(screen_w // 2 + nav_width, screen_h // 2 + browser_header_size)
+        shift = Point(width // 2, height // 2)  # (width / 2, height / 2) of the selected region to take a screenshot
+
+        top_left = Point(center.x - shift.x, center.y - shift.y)
+        bottom_right = Point(center.x + shift.x, center.y + shift.y)
+
+        # Take a screenshot of the region of interest
+        im = imagegrab.grab(bbox=(top_left.x, top_left.y, bottom_right.x, bottom_right.y))
+        # im.show()
+        im.save(save_to_file)
+
+    def close(self):
+        self.webdriver.close()
+
+
+if __name__ == "__main__":
+    # Specify Chrome driver path
+    options = Options()
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+
+    # Start Chrome
+    driver = webdriver.Chrome(options=options)
+
+    # Load the target webpage
+    url = 'https://livingatlas.arcgis.com/wayback/#active=47963&mapCenter=-83.78206%2C47.27444%2C19'
+    path = "./results/test_screenshot.png"
+
+    wayback_driver = ImageryWaybackDriver(driver)
+    wayback_driver.load_url(url)
+    wayback_driver.toggle_off_version_filter()
+    wayback_driver.accept_cookies()
+    release_dates = wayback_driver.get_release_dates()
+    wayback_driver.take_screenshot(512, 512, path)
+    # for key, value in release_dates.items():
+    #     print(f"Date: {key}; Release num: {value}")
