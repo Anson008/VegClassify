daf9bf8e2ab6e6cabea75e562dc208175f205589
Xingchen Zhao <xczhao007@gmail.com>
2024-04-30 12:02:31 -0600

    Move some functions to util.py

--- util.py
+++ util.py
@@ -4,6 +4,10 @@ import ctypes
 import cv2
 import numpy as np
 
+BETA = 1.1
+WAYBACK_SCALE_TO_RESOLUTION = {"16": 0.9843, "18": 0.2237, "17": 0.4474}
+TM_METHODS_STR = ('cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF_NORMED')
+
 
 class UnitConverterFactory:
     @staticmethod
@@ -77,3 +81,52 @@ def cv2_show_image(file_path, window_name, top_left=None, bottom_right=None, tra
     cv2.waitKey(0)
     cv2.destroyWindow(window_name)
 
+
+def get_wayback_shot_size(naip_size, naip_resolution, wayback_resolution):
+    naip_h, naip_w = naip_size
+    wayback_w = round(naip_h * naip_resolution / wayback_resolution * BETA)
+    wayback_h = round(naip_w * naip_resolution / wayback_resolution * BETA)
+    if wayback_w <= naip_w:
+        wayback_w = round(naip_w * BETA)
+    if wayback_h <= naip_h:
+        wayback_h = round(naip_h * BETA)
+    return wayback_h, wayback_w
+
+
+def get_template_matching_scales(naip_resolution, wayback_resolution, n_points=4):
+    # Alpha is the resizing factor that guarantees a Wayback imagery screenshot
+    # having the same resolution as NAIP imagery
+    alpha = naip_resolution / wayback_resolution
+    delta = alpha * (BETA - 1)
+    scales = np.linspace(alpha - delta, alpha + delta, n_points, endpoint=False)
+    return scales
+
+
+def get_ndvi_thresholds(start, stop, num):
+    return np.linspace(start, stop, num, endpoint=True)
+
+
+def get_naip_imagery_samples(naip_h, naip_w, n_samples_per_dim=10):
+    s_w = min(int(naip_w / n_samples_per_dim), 512)
+    s_h = min(int(naip_h / n_samples_per_dim), 512)
+
+    top_left_x = np.random.randint(0, naip_w - s_w, n_samples_per_dim)
+    top_left_y = np.random.randint(0, naip_h - s_h, n_samples_per_dim)
+    # print(f"x_t: {top_left_x}")
+    # print(f"y_t: {top_left_y}")
+    bottom_right_x = top_left_x + s_w
+    bottom_right_y = top_left_y + s_h
+    # print(f"x_b: {bottom_right_x}")
+    # print(f"y_b: {bottom_right_y}")
+
+    top_left_xy = np.array(np.meshgrid(top_left_x, top_left_y)).T.reshape(-1, 2)
+    bottom_right_xy = np.array(np.meshgrid(bottom_right_x, bottom_right_y)).T.reshape(-1, 2)
+
+    diagonal_xy = np.concatenate((top_left_xy, bottom_right_xy), axis=1)
+    # print(diagonal_xy)
+
+    return diagonal_xy
+
+
+if __name__ == '__main__':
+    get_naip_imagery_samples(400, 400, 2)
