f4c3c934a7a07c40b99dca72cc0478bf91f7d6a1
Xingchen Zhao <xczhao007@gmail.com>
2024-05-10 14:16:50 -0600

    Add a function to stitch images which were split from an image using split_image() method in NAIPProcessor

--- util.py
+++ util.py
@@ -1,8 +1,9 @@
 import math
 import ctypes
-
+import os
 import cv2
 import numpy as np
+import rioxarray as rxr
 
 BETA = 1.1
 WAYBACK_SCALE_TO_RESOLUTION = {"16": 0.9843, "18": 0.2237, "17": 0.4474}
@@ -106,12 +107,13 @@ def get_ndvi_thresholds(start, stop, num):
     return np.linspace(start, stop, num, endpoint=True)
 
 
-def get_naip_imagery_samples(naip_h, naip_w, n_samples_xy=(2, 2)):
+def get_naip_imagery_samples(naip_h, naip_w, n_samples_xy=(2, 2), seed=None):
     s_w = min(int(naip_w / n_samples_xy[0]), 512)
     s_h = min(int(naip_h / n_samples_xy[1]), 512)
 
-    top_left_x = np.random.randint(0, naip_w - s_w, n_samples_xy[0])
-    top_left_y = np.random.randint(0, naip_h - s_h, n_samples_xy[1])
+    rng = np.random.default_rng(seed)
+    top_left_x = rng.integers(0, naip_w - s_w, n_samples_xy[0], dtype=np.int32)
+    top_left_y = rng.integers(0, naip_h - s_h, n_samples_xy[1], dtype=np.int32)
 
     bottom_right_x = top_left_x + s_w
     bottom_right_y = top_left_y + s_h
@@ -125,5 +127,78 @@ def get_naip_imagery_samples(naip_h, naip_w, n_samples_xy=(2, 2)):
     return diagonal_xy
 
 
+def read_naip_image(image_path):
+    naip = None
+    try:
+        naip = rxr.open_rasterio(image_path)
+    except IOError as err:
+        print(err)
+    return naip
+
+
+def create_directory(directory):
+    if not os.path.exists(directory):
+        try:
+            os.makedirs(directory)
+        except OSError as err:
+            print(err)
+
+
+def remove_all_files(directory):
+    if len(os.listdir(directory)) == 0:
+        return
+    try:
+        with os.scandir(directory) as entries:
+            for entry in entries:
+                if entry.is_file():
+                    os.unlink(entry.path)
+        print(f"All cached NAIP crops are deleted at {directory}")
+    except OSError as err:
+        print(err)
+
+
+def naip_to_bgr(naip_img):
+    naip_np_arr = naip_img.values
+
+    # Extract RGB channels and reorder the color axis from (c, w, h) to (w, h, c)
+    naip_rgb = np.moveaxis(naip_np_arr[0:3], 0, -1)
+    if naip_rgb is None:
+        print("Naip image is empty")
+
+    # Convert RGB to BGR, as BGR is the default color model of OpenCV
+    return cv2.cvtColor(naip_rgb, cv2.COLOR_RGB2BGR)
+
+
+def stitch_images(in_dir, out_dir):
+    token = "rowSize"
+    count = 0
+    images = []
+    row_size = 0
+    try:
+        with os.scandir(in_dir) as entries:
+            sorted_entries = sorted(entries, key=lambda x: x.name)
+        for entry in sorted_entries:
+            # print(entry.name)
+            if entry.is_file():
+                if count == 0:
+                    tokens = entry.name.split("_")
+                    row_size = int(tokens[2][len(token):])
+                    print(row_size)
+                    count += 1
+                images.append(cv2.imread(entry.path))
+        img_rows = []
+        for start_idx in range(0, len(images), row_size):
+            # print(f"start_idx: {start_idx}")
+            img_row = np.hstack(tuple(np.asarray(i) for i in images[start_idx:start_idx + row_size]))
+            img_rows.append(img_row)
+        imgs_merged = np.vstack(tuple(img_rows))
+        create_directory(out_dir)
+        cv2.imwrite(os.path.join(out_dir, f"merged_ground_truth.png"), imgs_merged)
+    except OSError as err:
+        print(err)
+
+
 if __name__ == '__main__':
-    get_naip_imagery_samples(400, 400)
+    in_dir = "./cache/naip_split/"
+    out_dir = "./cache/naip_merged/"
+    stitch_images(in_dir, out_dir)
