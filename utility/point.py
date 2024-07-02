8bd175acc7cf3fc83e6a0c2a42de5c033ed20fa8
Xingchen Zhao <xczhao007@gmail.com>
2024-07-02 15:14:46 -0600

    Add type hints and docstring for Point class.

--- utility/point.py
+++ utility/point.py
@@ -1,23 +1,28 @@
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int):
+        """
+        Create an instance of the Point class
+        :param x: int, the x coordinate of the point
+        :param y: int, the y coordinate of the point
+        """
         self._x = x
         self._y = y
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Point ({self.x}, {self.y}"
 
     @property
-    def x(self):
+    def x(self) -> int:
         return self._x
 
     @x.setter
-    def x(self, val):
+    def x(self, val: int):
         self._x = val
 
     @property
-    def y(self):
+    def y(self) -> int:
         return self._y
 
     @y.setter
-    def y(self, val):
+    def y(self, val: int):
         self._y = val
